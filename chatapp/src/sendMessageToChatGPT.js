a8be553126180c0ac91f9834e8ba8d5848eedafa
creativival <creativival@gmail.com>
2024-04-08 20:08:31 +0900

    音声読み上げ関数を別のファイルに分離する

--- chatapp/src/sendMessageToChatGPT.js
+++ chatapp/src/sendMessageToChatGPT.js
@@ -1,52 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import speakWithOpenAI from './speakWithOpenAI';
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-
-const speakWithOpenAI = async (text, apiKey) => {
-  try {
-    // OpenAI APIを使用してテキストから音声を生成
-    const response = await axios.post(
-      'https://api.openai.com/v1/audio/speech',
-      {
-        "model": "tts-1",
-        "input": text,
-        "voice": "nova"
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-        responseType: 'arraybuffer',
-      }
-    );
-
-    // レスポンスからオーディオデータを取得
-    const audioData = response.data;
-
-    // Audioオブジェクトを作成
-    const audio = new Audio();
-
-    // オーディオデータをBlobに変換してソースに設定
-    const audioBlob = new Blob([audioData], {type: 'audio/mpeg'});
-    const audioUrl = URL.createObjectURL(audioBlob);
-    audio.src = audioUrl;
-
-    // 音声再生
-    await audio.play();
-
-    // 再生が終わるまで待機
-    await new Promise((resolve) => {
-      audio.onended = resolve;
-    });
-
-    console.log('TTS playback completed.');
-  } catch (error) {
-    console.error('Error:', error);
-    console.error("Sorry, I couldn't generate the TTS output.");
-  }
-};
 
 const sendMessageToChatGPT = async (messages, setMessages, SYSTEM_CONTENT, text) => {
   try {
