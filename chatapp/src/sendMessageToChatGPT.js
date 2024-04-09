649f09480a55174cc5e4b0457139f765fbc6ce01
creativival <creativival@gmail.com>
2024-04-09 18:58:38 +0900

    openaiパッケージを使用する

--- chatapp/src/sendMessageToChatGPT.js
+++ chatapp/src/sendMessageToChatGPT.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import OpenAI from 'openai';
 import speakWithOpenAI from './speakWithOpenAI';
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
@@ -16,21 +17,31 @@ const sendMessageToChatGPT = async (messages, setMessages, SYSTEM_CONTENT, text)
 
     messageData.push({role: 'user', content: text});
 
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      {
-        model: 'gpt-3.5-turbo',
-        messages: messageData,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-      }
-    );
+    // const response = await axios.post(
+    //   'https://api.openai.com/v1/chat/completions',
+    //   {
+    //     model: 'gpt-3.5-turbo',
+    //     messages: messageData,
+    //   },
+    //   {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': `Bearer ${apiKey}`,
+    //     },
+    //   }
+    // );
+    //
+    // const assistantMessage = response.data.choices[0].message.content;
 
-    const assistantMessage = response.data.choices[0].message.content;
+    // OpenAI APIの設定
+    const openai = new OpenAI({ apiKey,
+      dangerouslyAllowBrowser: true });
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: messageData
+    });
+
+    const assistantMessage = response.choices[0].message.content;
 
     setMessages((prevMessages) => [
       ...prevMessages,
--- chatapp/src/speakWithOpenAI.js
+++ chatapp/src/speakWithOpenAI.js
@@ -1,29 +1,39 @@
-import React from 'react';
 import axios from 'axios';
+import OpenAI from 'openai';
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
 const speakWithOpenAI = async (text) => {
   try {
     // OpenAI APIを使用してテキストから音声を生成
-    const response = await axios.post(
-      'https://api.openai.com/v1/audio/speech',
-      {
-        "model": "tts-1",
-        "input": text,
-        "voice": "nova"
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-        responseType: 'arraybuffer',
-      }
-    );
+    // const response = await axios.post(
+    //   'https://api.openai.com/v1/audio/speech',
+    //   {
+    //     "model": "tts-1",
+    //     "input": text,
+    //     "voice": "nova"
+    //   },
+    //   {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': `Bearer ${apiKey}`,
+    //     },
+    //     responseType: 'arraybuffer',
+    //   }
+    // );
+
+    // OpenAI APIの設定
+    const openai = new OpenAI({ apiKey,
+      dangerouslyAllowBrowser: true });
+    const response = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: "nova",
+      input: text,
+    });
 
     // レスポンスからオーディオデータを取得
-    const audioData = response.data;
+    const audioData = await response.arrayBuffer();
+    console.log('Audio data:', audioData);
 
     // Audioオブジェクトを作成
     const audio = new Audio();
